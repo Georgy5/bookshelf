d8097954ff43e1cc8acd6e29c248de1b6857c2d6
Karl Keller <kgk86@hotmail.com>
2023-03-17 08:16:57 +0100

    Update Books Index Spec - Fix typo

--- spec/requests/books/index_spec.rb
+++ spec/requests/books/index_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe "GET /books", type: :request do
   it "returns a list of books" do
     get "/books"
 
-    expect(last_response).to be_scuccsessful
+    expect(last_response).to be_successful
     expect(last_response.content_type).to eq("application/json; charset=utf-8")
 
     response_body = JSON.parse(last_response.body)
