15a3af440a29459dd137c8cf2fb13dfb78d9bf7a
Karl Keller <kgk86@hotmail.com>
2023-03-31 20:37:29 +0200

    Update books show action

--- app/actions/books/show.rb
+++ app/actions/books/show.rb
@@ -4,8 +4,25 @@ module Bookshelf
   module Actions
     module Books
       class Show < Bookshelf::Action
-        def handle(*, response)
-          response.body = self.class.name
+        include Deps["persistence.rom"]
+
+        params do
+          required(:id).value(:integer)
+        end
+
+        def handle(request, response)
+          book = rom.relations[:books].by_pk(
+            request.params[:id]
+          ).one
+
+          response.format = :json
+
+          if book
+            response.body = book.to_json
+          else
+            response.status = 404
+            response.body = {error: "not_found"}.to_json
+          end
         end
       end
     end
