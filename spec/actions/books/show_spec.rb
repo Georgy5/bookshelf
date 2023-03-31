90901473563e3329df78ceb03c09893ac8333612
Karl Keller <kgk86@hotmail.com>
2023-03-31 21:03:52 +0200

    Update Show Action Spec

--- spec/actions/books/show_spec.rb
+++ spec/actions/books/show_spec.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.describe Bookshelf::Actions::Books::Show do
-  let(:params) { Hash[] }
+  subject(:action) do
+    Bookshelf::Actions::Books::Index.new
+  end
 
-  it "works" do
-    response = subject.call(params)
+  it "returns a successful JSON response" do
+    response = subject.call(id: "23", "HTTP_ACCEPT" => "application/json")
+
     expect(response).to be_successful
+    expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
   end
 end
